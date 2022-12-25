6bf782fffbddf99613f8509369c02647b3ca2541
Kamal Mansour <Kamal_mansour2001@icloud.com>
2022-12-25 13:27:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+let bookList = [];
+
+window.addEventListener('load', () => {
+  getAll().then((apiBooks) => (bookList = apiBooks));
+});
+
+searchField.addEventListener('keyup', (e) =>
+  renderBookList(
+    bookList.filter(({ title, author }) => {
+      const searchTerm = e.target.value.toLowerCase();
+      return (
+        title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        author.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    })
+  )
+);
+
+function renderBookList(bookList) {
+  const existingElement = document.querySelector('.book-list'); 
+
+  const root = document.getElementById('root');
+  existingElement && root.removeChild(existingElement);
+
+  bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', BookList(bookList));
+
+    let bookinfo = document.querySelector(".book-list");
+
+    if (bookinfo){ /* kollar om listan inte är null*/
+        bookinfo.addEventListener("mouseover", function(e) {
+            if (e.target && e.target.matches("li.book-list__item")) {
+                let bookID = e.target.value
+                let book = bookList.find(book => book.id === bookID)
+                bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', bookDetails(book)); /* visar omslagsbild för boken, från bookdetails*/
+            }
+        });
+
+        bookinfo.addEventListener("mouseout", function(e) { /* gör så bilden försvinner när mussen inte pekar*/
+            let bookDetail = document.querySelector("#bookDetail");
+            if(bookDetail)
+                bookDetail.remove();
+        });
+    }
+}
+
+
+
