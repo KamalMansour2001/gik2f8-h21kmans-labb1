17b01c6e82e5fc00b1037bdb7378d9dbcc894f21
Kamal Mansour <Kamal_mansour2001@icloud.com>
2022-12-25 13:28:46 +0100

    fixing

--- script.js
+++ script.js
@@ -28,16 +28,16 @@ function renderBookList(bookList) {
 
     let bookinfo = document.querySelector(".book-list");
 
-    if (bookinfo){ /* kollar om listan inte är null*/
+    if (bookinfo){
         bookinfo.addEventListener("mouseover", function(e) {
             if (e.target && e.target.matches("li.book-list__item")) {
                 let bookID = e.target.value
                 let book = bookList.find(book => book.id === bookID)
-                bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', bookDetails(book)); /* visar omslagsbild för boken, från bookdetails*/
+                bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', bookDetails(book));
             }
         });
 
-        bookinfo.addEventListener("mouseout", function(e) { /* gör så bilden försvinner när mussen inte pekar*/
+        bookinfo.addEventListener("mouseout", function(e) {
             let bookDetail = document.querySelector("#bookDetail");
             if(bookDetail)
                 bookDetail.remove();
